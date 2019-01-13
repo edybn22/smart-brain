8d7a3258fdfae3d12edaccae3119cf5983188780
Edy Bn <bucatarueduardn@gmail.com>
2019-01-13 12:20:42 +0200

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://shielded-oasis-99360.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
